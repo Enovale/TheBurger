f53704a990ed5f2926b959f4c7fa3a7fe6aa9090
Elijah Bansley <>
2020-04-26 20:24:14 -0500

    Get rid of useless mats and add universal mat

--- Assets/TheBurger/Scripts/BurgerModule.cs
+++ Assets/TheBurger/Scripts/BurgerModule.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BurgerModule : MonoBehaviour {
+public class BurgerModule : MonoBehaviour 
+{
+	private KMAudio Audio;
+	private KMBombInfo Bomb;
+	private KMBombModule Module;
+	private KMSelectable Selectable;
 
-	// Use this for initialization
-	void Start () {
-		
+	public GameObject[] ProgressLEDs;
+
+	private KMSelectable[] buttons;
+
+	private void Start () {
+		Audio = GetComponent<KMAudio>();
+		Bomb = GetComponent<KMBombInfo>();
+		Module = GetComponent<KMBombModule>();
+		Selectable = GetComponent<KMSelectable>();
+
+		buttons = Selectable.Children;
+		foreach(KMSelectable button in buttons)
+		{
+			button.OnInteract += () => { button.AddInteractionPunch(); return false; };
+		}
+
+		Init();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Init()
+	{
+		// sutb
 	}
 }
